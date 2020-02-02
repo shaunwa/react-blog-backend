8e08543a2738ad7c5f816d0e52ea17fea0d37a22
Shaun Wassell <shaun@trove.ai>
2020-02-02 14:22:48 -0500

    02.04 - Added fake articles database and upvote endpoint

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+    'learn-react': { upvotes: 0 },
+    'learn-node': { upvotes: 0 },
+    'my-thoughts-on-resumes': { upvotes: 0 },
+};
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
-
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
-
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send('Success! ' + articleName + ' now has ' + articlesInfo[articleName].upvotes + ' upvotes!');
+});
 
 app.listen(8000, () => console.log('Server is listening on port 8000'));
