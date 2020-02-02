569eb7c2af32010c186f01812bf61b0d5e794d5e
Shaun Wassell <shaun@trove.ai>
2020-02-02 15:41:33 -0500

    02.06 - Added a comments field to the articles and an add-comment endpoint

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const articlesInfo = {
-    'learn-react': { upvotes: 0 },
-    'learn-node': { upvotes: 0 },
-    'my-thoughts-on-resumes': { upvotes: 0 },
+    'learn-react': { upvotes: 0, comments: [] },
+    'learn-node': { upvotes: 0, comments: [] },
+    'my-thoughts-on-resumes': { upvotes: 0, comments: [] },
 };
 
 const app = express();
@@ -19,6 +19,14 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articlesInfo[articleName].upvotes += 1;
     res.status(200).send('Success! ' + articleName + ' now has ' + articlesInfo[articleName].upvotes + ' upvotes!');
+});
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const articleName = req.params.name;
+    const newComment = req.body.comment;
+
+    articlesInfo[articleName].comments.push(newComment);
+
+    res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log('Server is listening on port 8000'));
