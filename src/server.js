a35e54b7fe636c1de9d3604dcf6dd1af58d7bf5e
Shaun Wassell <shaun@trove.ai>
2020-02-02 14:07:12 -0500

    02.03b - Added a GET /hello/:name route

--- src/server.js
+++ src/server.js
@@ -9,4 +9,6 @@ app.get('/hello', (req, res) => res.send('Hello!'));
 
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
+
 app.listen(8000, () => console.log('Server is listening on port 8000'));
