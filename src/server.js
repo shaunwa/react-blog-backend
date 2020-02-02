d91c8e2d44a360c872c1a7b3e6f9f4984170c1f3
Shaun Wassell <shaun@trove.ai>
2020-02-02 09:34:28 -0500

    02.02 - Created a basic server with a hello route

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+
+app.listen(8000, () => console.log('Server is listening on port 8000'));
