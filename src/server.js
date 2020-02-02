433667b3887eccb54945ac99f7680acfb6b76744
Shaun Wassell <shaun@trove.ai>
2020-02-02 15:59:21 -0500

    03.03 - Added mongodb driver and GET /api/articles/:name route

--- src/server.js
+++ src/server.js
@@ -1,20 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-    'learn-react': { upvotes: 0, comments: [] },
-    'learn-node': { upvotes: 0, comments: [] },
-    'my-thoughts-on-resumes': { upvotes: 0, comments: [] },
-};
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
 
+const withDB = async (operations) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('react-blog-db');
+
+        await operations(db);
+
+        client.close();
+    } catch (err) {
+        res.status(500).send({ message: 'Database Error', err });
+    }
+}
+
 app.get('/hello', (req, res) => res.send('Hello!'));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 
+app.get('/api/articles/:name', async (req, res) => {
+    const articleName = req.params.name;
+
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('react-blog-db');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (err) {
+        res.status(500).send({ message: 'Database Error', err });
+    }
+});
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articlesInfo[articleName].upvotes += 1;
