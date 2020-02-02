8e971f8c4e7532def91aeeedc962e13e409a5bd5
Shaun Wassell <shaun@trove.ai>
2020-02-02 14:01:46 -0500

    02.03 - Added POST /hello route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => res.send('Hello!'));
+
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Server is listening on port 8000'));
