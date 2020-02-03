e59030f7886c55d8a7e9bec4f2c78c5cef9eb1b3
Shaun Wassell <shaun@trove.ai>
2020-02-02 23:21:42 -0500

    03.05 - Rewrote upvote endpoint to use MongoDB

--- src/server.js
+++ src/server.js
@@ -6,19 +6,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const withDB = async (operations) => {
-    try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('react-blog-db');
-
-        await operations(db);
-
-        client.close();
-    } catch (err) {
-        res.status(500).send({ message: 'Database Error', err });
-    }
-}
-
 app.get('/hello', (req, res) => res.send('Hello!'));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
@@ -38,10 +25,24 @@ app.get('/api/articles/:name', async (req, res) => {
         res.status(500).send({ message: 'Database Error', err });
     }
 });
-app.post('/api/articles/:name/upvote', (req, res) => {
+app.post('/api/articles/:name/upvote', async (req, res) => {
     const articleName = req.params.name;
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send('Success! ' + articleName + ' now has ' + articlesInfo[articleName].upvotes + ' upvotes!');
+
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('react-blog-db');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, { '$set': {
+            upvotes: articleInfo.upvotes + 1,
+        }});
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+
+        client.close();
+    } catch (err) {
+        res.status(500).send({ message: 'Database Error', err });
+    }
 });
 app.post('/api/articles/:name/add-comment', (req, res) => {
     const articleName = req.params.name;
