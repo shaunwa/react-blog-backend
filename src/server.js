90bac4c431a6c73e3747a242a99b91c611008929
Shaun Wassell <shaun@trove.ai>
2020-02-02 23:37:14 -0500

    03.05 - Rewrote endpoints to use withDB function instead of repeating boilerplate, also rewrote add-comment endpoint

--- src/server.js
+++ src/server.js
@@ -6,51 +6,54 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const withDB = async operations => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('react-blog-db');
+
+        await operations(db);
+
+        client.close();
+    } catch (err) {
+        res.status(500).send({ message: 'Database Error', err });
+    }
+}
+
 app.get('/hello', (req, res) => res.send('Hello!'));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 
 app.get('/api/articles/:name', async (req, res) => {
     const articleName = req.params.name;
-
-    try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
-        const db = client.db('react-blog-db');
-
+    await withDB(async db => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(articleInfo);
-
-        client.close();
-    } catch (err) {
-        res.status(500).send({ message: 'Database Error', err });
-    }
+    });
 });
 app.post('/api/articles/:name/upvote', async (req, res) => {
     const articleName = req.params.name;
 
-    try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
-        const db = client.db('react-blog-db');
-
+    await withDB(async db => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, { '$set': {
             upvotes: articleInfo.upvotes + 1,
         }});
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
-
-        client.close();
-    } catch (err) {
-        res.status(500).send({ message: 'Database Error', err });
-    }
+    });
 });
-app.post('/api/articles/:name/add-comment', (req, res) => {
+app.post('/api/articles/:name/add-comment', async (req, res) => {
     const articleName = req.params.name;
     const newComment = req.body.comment;
 
-    articlesInfo[articleName].comments.push(newComment);
-
-    res.status(200).send(articlesInfo[articleName]);
+    await withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, { '$set': {
+            comments: articleInfo.comments.concat(newComment),
+        }});
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+    });
 });
 
 app.listen(8000, () => console.log('Server is listening on port 8000'));
